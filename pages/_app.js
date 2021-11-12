d7590ff1c58df14d60b44782d3e1abd611e239db
thinceller <thinceller@gmail.com>
2021-11-12 12:12:19 +0900

    Add Initializer

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,52 @@
+import { useRouter } from "next/dist/client/router";
 import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
+import useSWR from "swr";
 import { useNameState, useSetNameState } from "../lib/recoil";
 
-function Initializer() {
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+function Initializer(props) {
+  const router = useRouter();
+  if (router.pathname === '/no-auth') {
+    return <>{props.children}</>
+  }
+
+  return <Authorizer>{props.children}</Authorizer>;
+}
+
+function Authorizer(props) {
+  const router = useRouter();
   const name = useNameState();
   const setName = useSetNameState();
+  const { data, error } = useSWR(name ? null : '/api/hello', fetcher)
 
   useEffect(() => {
-    if (name === null) {
-      setName("Akira");
+    if (name === null && data) {
+      setName(data.name);
     }
-  }, [name, setName]);
+  }, [name, setName, data]);
 
-  return null;
+  console.log({ name, data })
+
+  if (error) {
+    router.replace('/no-auth')
+    return <div>error</div>;
+  }
+
+  if (name === null) {
+    return <div>loading...</div>;
+  }
+
+  return <>{props.children}</>;
 }
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
-      <Initializer />
+      <Initializer>
+        <Component {...pageProps} />
+      </Initializer>
     </RecoilRoot>
   );
 }
---
+++ pages/no-auth.js
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default function NoAuth() {
+  return <div>no auth page <Link href="/">Home</Link></div>;
+}
